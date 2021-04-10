829e458382b44c98885c15f9b747a33843e0cc43
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-04-10 10:10:54 -0500

    app-2

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
+from datetime import datetime
 from flask import Flask, jsonify
-
+import datetime as dt
 
 #################################################
 # Database Setup
@@ -27,6 +27,7 @@ session=Session(engine)
 #################################################
 app = Flask(__name__)
 
+oneYearFromDate= '2016-08-23'
 
 #################################################
 # Flask Routes
@@ -41,6 +42,11 @@ def Welcome():
 
     )
 
+#Convert the query results to a dictionary using date as the key and prcp as the value.
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prcp_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > oneYearFromDate).order_by(Measurement.date).all()
+    return jsonify(prcp_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
